1870bd2cd67cdca6fd3e7bcf7d2a073aaff998a5
AlexanderWeinberg <alexw.dagreat@gmail.com>
2019-11-07 15:12:32 -0500

    Initialize saved registers and exits program

--- project2.s
+++ project2.s
@@ -9,10 +9,17 @@ invalid: .asciiz "Invalid input"  #makes asciiz Invalid message
 
 main:
 
+#saved registers
+la $s1, 0     #register to keep track of final output
+la $s2, 0     #register to keep track of when a character is found
+la $s3, 0     #register to keep track of spaces
+
+
 Invalid: 	#runs if the character is invalid
 li $v0, 4        #prints out a string
 la $a0, invalid    #prints the invalid input message
 syscall
 
-
+li $v0, 10    #exits the program
+syscall
 
