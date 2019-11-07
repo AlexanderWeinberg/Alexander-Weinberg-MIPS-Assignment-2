676143c3f50649fc70024074ecc481b620b9420d
AlexanderWeinberg <alexw.dagreat@gmail.com>
2019-11-07 15:08:20 -0500

    Prints invalid input message to console

--- project2.s
+++ project2.s
@@ -3,10 +3,16 @@
 .data
 user_input: .space 1000	   #makes 1000 spaces for the user input
 endl: .asciiz "\n"	   #makes asciiz character for a new line
+invalid: .asciiz "Invalid input"  #makes asciiz Invalid message
 
 .text
 
 main:
+
+Invalid: 	#runs if the character is invalid
+li $v0, 4        #prints out a string
+la $a0, invalid    #prints the invalid input message
+syscall
 
 
 
